6660d86cd504d4c8b54eb81852ecfd375d3947da
arielpiccolo <arielpiccolo@gmail.com>
2020-06-16 16:10:10 +0100

    update

--- netlix_activity/src/App.js
+++ netlix_activity/src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import '../src/App.css';
+import '../src/App.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+
+        <h1 className="title">CodeNation Netflix FAQ Activity</h1>
+
+        <div className="outerContainer">
+
+          <div className="innerContainer">
+
+            <h1 className="faq">Frequently Asked Questions</h1>
+
+            <ul>
+              <li className="liText">What is Netflix?<p class="plus">+</p></li>
+              <li className="liText">How much does Netflix cost?<p class="plus">+</p></li>
+              <li className="liText">Where can I watch?<p class="plus">+</p></li>
+              <li className="liText">How do I cancel?<p class="plus">+</p></li>
+              <li className="liText">What can I watch on Netflix?<p class="plus">+</p></li>
+              <li className="liText">How does the free trial work?<p class="plus">+</p></li>
+            </ul>
+
+            <div className="try">
+              <p className="tryText" > Try 30 Days FREE</p>
+            </div>
+
+
+          </div>
+
+        </div>
+
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
